b5c37099d6ea1992d7583cb8edf1e13ed916d0bc
bursachelio <bursak.99@mail.ru>
2023-06-05 17:57:04 +0300

    Добавил статическую страницу about/author с информацией об авторе

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'static_pages/about'
+  get 'static_pages/author'
   root "static_pages#about"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
