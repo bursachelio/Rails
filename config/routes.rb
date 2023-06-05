e03d649c007d7d7504e7fb4e2dfbb8ea71e89c54
bursachelio <bursak.99@mail.ru>
2023-06-05 17:33:38 +0300

    Добавил статическую страницу /about с кратким описанием проекта, включая ссылку на сайт онлайн-школы Thinknetica (www.thinknetica.com)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'static_pages/about'
+  root "static_pages#about"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
