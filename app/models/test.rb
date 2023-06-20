ecf684887d536236eb19c1d847782718969b88ab
bursachelio <bursak.99@mail.ru>
2023-06-20 17:42:03 +0300

    исправил классы User и Test

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Test < ApplicationRecord
-  def self.sorted_tests_by_category(category_name)
-    joins(:category).where(categories: { title: category_name }).order(title: :desc).pluck(:title)
+  def self.sorted_test_titles_by_category_id(category_id)
+    where(category_id: category_id).order(title: :desc).pluck(:title)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_many :tests, foreign_key: 'author_id'
+  
   def tests_by_difficulty(level)
-    tests.where(tests: { level: level })
+    tests.where(level: level)
   end
 end
