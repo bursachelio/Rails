38b1dffce9dcd9d6888ceb1589690f91f66da787
bursachelio <bursak.99@mail.ru>
2023-06-20 17:55:00 +0300

    Убрал ассоциации из User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  has_many :tests, foreign_key: 'author_id'
-  
   def tests_by_difficulty(level)
-    tests.where(level: level)
+    Test.where(author_id: id, level: level)
   end
 end
