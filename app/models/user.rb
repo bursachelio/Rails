f47452b63a856045295f5208bcced1da9b9a0c24
bursachelio <bursak.99@mail.ru>
2023-06-22 11:33:06 +0300

    исправил модели User и Test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  def tests_by_difficulty(level)
-<<<<<<< HEAD
-    tests.joins(:results).where(results: { level: level })
-=======
-    Test.where(user_id: id, level: level).order(title: :asc)
->>>>>>> f9c5bef30da2e8f636da6260491690f5c333fcd7
+  def tests_at_difficulty_level(level)
+    Test.joins("INNER JOIN users ON users.id = tests.user_id")
+        .where("users.id = ? AND tests.level = ?", id, level)
   end
 end
