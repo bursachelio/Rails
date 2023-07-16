8983509fcfcd71520c6123308449439fbe7f5725
bursachelio <bursak.99@mail.ru>
2023-07-16 17:46:41 +0300

    Определил ассоциации в моделях

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
 class Test < ApplicationRecord
+  belongs_to :category
+  has_many :questions
+  has_many :results
+  has_many :users, through: :results
+  has_many :results
+  has_many :tests, through: :results
+  belongs_to :author, class_name: 'User'
   def self.sorted_test_titles_by_category_name(category_name)
     joins('JOIN categories ON categories.id = tests.category_id')
       .where(categories: { title: category_name })
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_many :tests, foreign_key: 'author_id'
   def tests_at_difficulty_level(level)
     Test.joins('INNER JOIN results ON results.test_id = tests.id')
         .where(results: { user_id: id }, level: level)
