75d2a91ccb039ebca0995a595a3d7658645fba54
bursachelio <bursak.99@mail.ru>
2023-07-31 14:39:07 +0300

    Добавлены хелперы

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,19 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def question_header(question, test)
+    if question.new_record?
+      "Create New #{test.title} Question"
+    else
+      "Edit #{test.title} Question"
+    end
+  end
+
+  def current_year
+    Time.now.year
+  end
+
+  def github_url(author, repo)
+    "https://github.com/#{author}/#{repo}"
+  end
 end
