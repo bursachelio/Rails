020eb21ae2d96fe05e21d9563525c91fe43422e2
bursachelio <bursak.99@mail.ru>
2023-08-04 17:01:02 +0300

    работает просмотр, редактирование тестов, удаление и создание нового не работает

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,41 +1,30 @@
-# frozen_string_literal: true
-
 class QuestionsController < ApplicationController
-  before_action :find_question, only: [:show, :destroy]
+  before_action :find_test, only: [:index, :new, :create]
 
   def index
     @questions = @test.questions
   end
 
-  def show
-    render plain: @question.content, status: :ok
-  end
-
   def new
-    @question = @test.questions.new
+    @question = @test.questions.build
   end
 
   def create
-    @question = @test.questions.new(question_params)
+    @question = @test.questions.build(question_params)
     if @question.save
-      redirect_to @question, notice: "Вопрос успешно создан!"
+      redirect_to test_questions_path(@test)
     else
       render :new
     end
   end
 
-  def destroy
-    @question.destroy
-    render plain: "Вопрос успешно удален!"
-  end
-
   private
 
-  def find_question
-    @question = Question.find(params[:id])
+  def find_test
+    @test = Test.find(params[:test_id])
   end
 
   def question_params
-    params.require(:question).permit(:content)
+    params.require(:question).permit(:content, :test_id)
   end
 end
--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,14 +1,11 @@
-
 class TestsController < ApplicationController
-  before_action :find_test, only: [:show, :edit, :update, :destroy]
 
   def index
     @tests = Test.all
   end
 
   def show
-    @questions = @test.questions
-    logger.debug "Questions: #{@questions}"
+    @test = Test.find(params[:id])
   end
 
   def new
@@ -18,18 +15,20 @@ class TestsController < ApplicationController
   def create
     @test = Test.new(test_params)
     if @test.save
-      redirect_to @test, notice: 'Тест успешно создан!'
+      redirect_to @test
     else
       render :new
     end
   end
 
   def edit
+    @test = Test.find(params[:id])
   end
 
   def update
+    @test = Test.find(params[:id])
     if @test.update(test_params)
-      redirect_to @test, notice: 'Тест успешно обновлен!'
+      redirect_to @test
     else
       render :edit
     end
@@ -38,16 +37,12 @@ class TestsController < ApplicationController
   def destroy
     @test = Test.find(params[:id])
     @test.destroy
-    redirect_to tests_path, notice: "Тест успешно удален!"
+    redirect_to tests_path
   end
 
   private
 
-  def find_test
-    @test = Test.find(params[:id])
-  end  
-
   def test_params
-    params.require(:test).permit(:title, :level, :category_id, :author_id)
+    params.require(:test).permit(:title, :level, :category_id)
   end
 end
